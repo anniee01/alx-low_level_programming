f9287f20979097b83c9d674efa14f265b8a7b10d
anniee01 <waiyu94@gmail.com>
2022-04-09 13:46:29 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
+
 /**
-<<<<<<< HEAD
-*main - prints the phrase "Programming is like building a multilingual 
-puzzle
-*Return: 0 if exited properly, non-zero otherwise
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success
 */
+
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-=======
- *main - prints the phrase "Programming is like building multilingual puzzle
- *Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
->>>>>>> 56f8f69ce0e0ef050a52ffd9d38ad3e5c93438df
+
 }
